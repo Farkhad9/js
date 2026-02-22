836fa79529825cac19ee96cf915d758be704c802
Farkhad9 <farhadai@code.edu.az>
2026-02-22 20:32:52 +0400

    js var let const

---
+++ script.js
@@ -0,0 +1,38 @@
+
+'use strict'
+
+
+// console.log('Привет, мир!')
+// console.log('Привет, мир!')
+// let message = 'Привет'
+// console.log(message)
+
+// message = 'Пока'
+// console.log(message)
+
+
+// const message = 'Вася'
+
+// message = 'Петя' // Ошибка, так как константа не может быть изменена
+// console.log(message)
+
+
+
+
+// let message = 'Farkhad';
+// const age = 25
+// const isProgrammer = true
+
+// console.log(message)
+// console.log(age)
+// console.log(isProgrammer)
+
+
+// const name = 'Farkhad';
+// const admin = name;
+
+// console.log(admin)
+
+const age = 25
+console.log(age)
+
