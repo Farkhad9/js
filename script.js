91c3a82830c17e89c43cf686793972107f568fe1
Farkhad9 <farhadai@code.edu.az>
2026-02-25 23:20:28 +0400

    js data types arrays

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-
-'use strict'
-
-
-// console.log('Привет, мир!')
+    // console.log('Привет, мир!')
 // console.log('Привет, мир!')
 // let message = 'Привет'
 // console.log(message)
@@ -38,29 +34,59 @@
 
 //------------------------------ UROK 3---------------------------------------------
 
-const greeting = 'Привет'
-const name = "Александр"
-const goodbyeMessage = 'До свидания'
-const message = `${greeting}, ${name}! ${goodbyeMessage}`
-console.log(message)
+// const greeting = 'Привет'
+// const name = "Александр"
+// const goodbyeMessage = 'До свидания'
+// const message = `${greeting}, ${name}! ${goodbyeMessage}`
+// console.log(message)
 
 
-const a = 5
-const b = 12
-const sum = `summa cisel a i b ravna ${a+b}`
-console.log(sum)
+// const a = 5
+// const b = 12
+// const sum = `summa cisel a i b ravna ${a+b}`
+// console.log(sum)
 
-const age = 25
-const ischild = age < 18
-console.log(ischild)
+// const age = 25
+// const ischild = age < 18
+// console.log(ischild)
 
 
-const num =111
-const str = '222'
-const resukt = num + str
-console.log(resukt) // Результат будет '111222', так как при сложении числа и строки число преобразуется в строку.
-console.log(typeof resukt) // Результат будет 'string', так как результат сложения - это строка.
+// const num =111
+// const str = '222'
+// const resukt = num + str
+// console.log(resukt) // Результат будет '111222', так как при сложении числа и строки число преобразуется в строку.
+// console.log(typeof resukt) // Результат будет 'string', так как результат сложения - это строка.
 
+// console.log (typeof('14' / '13' ) 
+// )
+
+
+
+
+
+// code academy
+
+
+// alert ('Privet')
+// prompt ('Kak tebya zovut?')
+
+
+// var let const
+
+
+
+var number = "123"
+console.log(typeof number)
+
+
+let numbers = [22,44,55,6,62]
+// numbers.push(100)
+// numbers.pop()
+// numbers.shift()
+numbers.unshift(100)
+console.log(numbers)
+
+<<<<<<< HEAD
 console.log (typeof('14' / '13' ) 
 )
 
@@ -94,4 +120,3 @@ if (num % 2 === 0) {
 } else {
     console.log("Нечётное число");
 }
-
