4a29876d93470e54a620bbaf53fcebfcff02e14d
Farkhad9 <farhadai@code.edu.az>
2026-03-03 02:36:18 +0400

    js urok 14

--- script.js
+++ script.js
@@ -1,269 +1,51 @@
-// console.log('Привет, мир!')
-// console.log('Привет, мир!')
-// let message = 'Привет'
-// console.log(message)
+// copy meselesi
 
-// message = 'Пока'
-// console.log(message)
+const user = {
+    name: "John",
+    age: 30,
+    work : "Developer"
+  }
+// let user1= user
+// user1.name = "Jane"
+// console.log(user.name)
 
+// let user1={... user}   // 1 copy
 
-// const message = 'Вася'
-
-// message = 'Петя' // Ошибка, так как константа не может быть изменена
-// console.log(message)
-
-
-
-
-// let message = 'Farkhad';
-// const age = 25
-// const isProgrammer = true
-
-// console.log(message)
-// console.log(age)
-// console.log(isProgrammer)
+// let user1 = JSON.stringify(user)  // 2 copy
+// let user2 = JSON.parse(user1)  // 3 copy
+// let user1 = Object.assign({//mojno dobavit cto to iz sebya}, user)  // 4 copy
 
 
-// const name = 'Farkhad';
-// const admin = name;
+// object yaratma usullari
 
-// console.log(admin)
-
-// const age = 25
-// console.log(age)
-
-//------------------------------ UROK 3---------------------------------------------
-
-// const greeting = 'Привет'
-// const name = "Александр"
-// const goodbyeMessage = 'До свидания'
-// const message = `${greeting}, ${name}! ${goodbyeMessage}`
-// console.log(message)
-
-
-// const a = 5
-// const b = 12
-// const sum = `summa cisel a i b ravna ${a+b}`
-// console.log(sum)
-
-// const age = 25
-// const ischild = age < 18
-// console.log(ischild)
-
-
-// const num =111
-// const str = '222'
-// const resukt = num + str
-// console.log(resukt) // Результат будет '111222', так как при сложении числа и строки число преобразуется в строку.
-// console.log(typeof resukt) // Результат будет 'string', так как результат сложения - это строка.
-
-// console.log (typeof('14' / '13' ) 
-// )
+// let person = new Object()
+// person.name = "Alice"
+// person.age = 25
 
 
 
-
-
-// code academy
-
-
-// alert ('Privet')
-// prompt ('Kak tebya zovut?')
-
-
-// var let const
-
-
-
-// var number = "123"
-// console.log(typeof number)
-
-
-// let numbers = [22, 44, 55, 6, 62]
-// // numbers.push(100)
-// // numbers.pop()
-// // numbers.shift()
-// numbers.unshift(100)
-// console.log(numbers)
-
-// <<<<<<< HEAD
-// console.log(typeof ('14' / '13')
-// )
-
-
-// let number = 10;
-
-// if (number > 0) {
-//     console.log("Число положительное");
-// } else {
-//     console.log("Число отрицательное или равно нулю");
-// }
-
-
-// let score = 75;
-
-// if (score >= 90) {
-//     console.log("Отлично");
-// } else if (score >= 70) {
-//     console.log("Хорошо");
-// } else if (score >= 50) {
-//     console.log("Удовлетворительно");
-// } else {
-//     console.log("Не сдал");
-// }
-
-
-// let num = 8;
-
-// if (num % 2 === 0) {
-//     console.log("Чётное число");
-// } else {
-//     console.log("Нечётное число");
-// }
-
-// const person = {
+// let person = {
 //     name: "Alice",
 //     age: 25,
-//     city: "New York"
-// };
-
-// const car = {
-//     brand: "Toyota",
-//     speed: 0,
-
-//     accelerate(amount) {
-//         this.speed += amount;
-//         console.log(`Speed: ${this.speed} km/h`);
-//     },
-
-//     brake(amount) {
-//         this.speed -= amount;
-//         console.log(`Speed: ${this.speed} km/h`);
-//     }
-// };
-
-// function Animal(name, sound) {
-//     this.name = name;
-//     this.sound = sound;
-
-//     this.speak = function () {
-//         console.log(`${this.name} says ${this.sound}!`);
-//     };
 // }
 
-// const dog = new Animal("Dog", "Woof");
-// dog.speak();
-
-// class Rectangle {
-//     constructor(width, height) {
-//         this.width = width;
-//         this.height = height;
-//     }
-
-//     area() {
-//         return this.width * this.height;
-//     }
-
-//     perimeter() {
-//         return 2 * (this.width + this.height);
-//     }
-// }
-
-// const rect = new Rectangle(10, 5);
-// console.log(rect.area());
-// console.log(rect.perimeter());
-
-// const greet = (name) => `Hello, ${name}!`;
-// console.log(greet("Bob"));
 
 
-const { name, age } = person;
-console.log(name, age);
+// let person = ({
+//     name: "Alice",
+//     age: 25,
+// })
 
 
-let BankAccount = {
-  owner: "Farkhad",
-  balance: 1000,
 
-  deposit(amount) {
-    this.balance += amount;
-    console.log("Пополнение на " + amount);
-  },
-
-  withdraw(amount) {
-    if (amount <= this.balance) {
-      this.balance -= amount;
-      console.log("Снятие " + amount);
-    } else {
-      console.log("Недостаточно средств");
-    }
-  },
-
-  checkBalance() {
-    console.log("Баланс: " + this.balance);
+class Person {
+  constructor (name, age) {
+    this.name = name
+    this.age = age
   }
-};
-
-
-function BankAccount(owner, balance) {
-  this.owner = owner;
-  this.balance = balance;
-
-  this.deposit = function(amount) {
-    this.balance += amount;
-  };
-
-  this.withdraw = function(amount) {
-    if (amount <= this.balance) {
-      this.balance -= amount;
-    }
-  };
+  getInfo=() => {
+    return `Name: ${this.name}, Age: ${this.age}`
+  }
 }
-
-let acc1 = new BankAccount("Ali", 2000);
-let acc2 = new BankAccount("Murad", 500);
-
-acc1.deposit(300);
-console.log(acc1.balance);
-
-
-// const { name, age } = person;
-// console.log(name, age);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ===============================================
-
-
-
-document.getElementById("box");        
-document.querySelector(".text");         
-document.querySelectorAll(".text");       
-document.getElementsByClassName("text"); 
-document.getElementsByTagName("p");      
-
-
-
-const para = document.getElementById("para");
-para.style.color = "red";
-para.style.fontSize = "24px";
-para.style.backgroundColor = "yellow";
-
-document.getElementById("title").textContent = "New Title";  
-document.getElementById("box").innerHTML = "<b>Bold Text</b>"; 
+let person1 = new Person("Alice", 25)
+console.log(person1.getInfo())
